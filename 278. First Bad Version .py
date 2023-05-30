d36ad952986cc7de5a216f66e5383506c3af4fb5
Enes Erdoğan <81412206+eneserdogann@users.noreply.github.com>
2023-05-30 21:12:13 +0300

    Add files via upload

---
+++ 278. First Bad Version .py
@@ -0,0 +1,9 @@
+def firstBadVersion(n):
+    l, r = 1, n
+    while l < r:
+        mid = l + (r - l) // 2
+        if isBadVersion(mid):
+            r = mid
+        else:
+            l = mid + 1
+    return l
